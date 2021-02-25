690388b198c51da7ac7eb24665d109f1a4e12129
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-02-25 12:02:39 +0530

    Added

--- BackTracking/Print All Permutations Of A String/Permutations_of_a_given_string.cpp
+++ BackTracking/Print All Permutations Of A String/Permutations_of_a_given_string.cpp
@@ -1,11 +1,20 @@
+#include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<string> permute(string a,vector<string>ans)
+vector<string> permute(string a,int l,int r,vector<string>ans)
 {
-    do{
-		ans.push_back(a);
-	}while(next_permutation(a.begin(),a.end()));
+    if(l==r)
+        ans.push_back(a);
+    else
+    {
+        for(int i=l;i<=r;i++)
+        {
+            swap(a[l],a[i]);
+            ans=permute(a,l+1,r,ans);
+            swap(a[l],a[i]);
+        }
+    }
     return ans;
 }
 
@@ -18,7 +27,7 @@ int main() {
 	    string a;
 	    cin >> a;
 	    int n=a.size();
-	    ans=permute(a,ans);
+	    ans=permute(a,0,n-1,ans);
 	    sort(ans.begin(),ans.end());
 	    for(int i=0;i<ans.size();i++)
 	        cout << ans[i] << " ";
