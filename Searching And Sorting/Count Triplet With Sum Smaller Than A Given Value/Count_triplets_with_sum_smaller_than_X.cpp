ca79b0009cbf788d555829cc22edd04ade13daa3
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-03-01 00:03:00 +0530

    Added

---
+++ Searching And Sorting/Count Triplet With Sum Smaller Than A Given Value/Count_triplets_with_sum_smaller_than_X.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+	
+	
+	public:
+	long long countTriplets(long long arr[], int n, long long sum)
+	{
+	    // Your code goes here
+	    long long ans=0;
+	    sort(arr,arr+n);
+	    for(int i=0;i<n;i++)
+	    {
+	        int j=i+1,k=n-1;
+	        while(j<k)
+	        {
+	            long long add=arr[i]+arr[j]+arr[k];
+	            if(add<sum)
+	            {
+	                ans+=k-j;
+	                j++;
+	            }
+	            else
+	                k--;
+	        }
+	    }
+	    return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+  
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+    	long long sum;
+        cin>>n>>sum;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+	    
+
+       
+
+        Solution ob;
+        cout << ob.countTriplets(arr, n, sum) ;
+	   
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
