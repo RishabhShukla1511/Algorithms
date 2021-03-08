6c490b7f38f51ebf51f48ca7711c537f701f8df4
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-03-08 09:46:49 +0530

    Added Next Greater Element

--- Stacks & Queues/Find The Next Greater Element/Next_greater_element.cpp
+++ Stacks & Queues/Find The Next Greater Element/Next_greater_element.cpp
@@ -1,40 +1,52 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
+
+
+ // } Driver Code Ends
+// Function to return Next larger elements
+
+
+class Solution{
+    public:
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long>ans;
+        for(int i=0;i<n;i++)
+            ans.push_back(-1);
+        stack<long long>s;
+        for(int i=0;i<n;i++)
+        {
+            while(!s.empty() && arr[s.top()]<arr[i])
+            {
+                ans[s.top()]=arr[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
 int main()
 {
     int t;
-    cin >> t;
+    cin>>t;
     while(t--)
     {
+        
         int n;
-        cin >> n;
-        ll arr[n];
+        cin>>n;
+        vector<long long> arr(n);
         for(int i=0;i<n;i++)
-            cin >> arr[i];
-        stack<ll>s;
-        ll ans[n];
-        for(int i=0;i<n;i++)
-        {
-            if(s.size()==0)
-                s.push(i);
-            else
-            {
-                while(s.size()!=0 && arr[s.top()]<arr[i])
-                {
-                    ans[s.top()]=arr[i];
-                    s.pop();
-                }
-                s.push(i);
-            }
-        }
-        while(s.size()!=0)
-        {
-            ans[s.top()]=-1;
-            s.pop();
-        }
-        for(int i=0;i<n;i++)
-            cout << ans[i] << " ";
-        cout << '\n';
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
     }
+	return 0;
 }
+  // } Driver Code Ends
