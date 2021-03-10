b7d5ef83c0bd6d7acc7cf8861416d86fa1e958ec
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-03-10 23:49:17 +0530

    Added Sort a ll of 0s, 1s and 2s

---
+++ Linked List/Sort A LL Of 0's, 1's And 2's/Given_a_linked_list_of_0s,_1s_and_2s,_sort_it(2).cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *start = NULL;
+
+struct Node* segregate(struct Node *head);
+
+// Function to sort a linked list of 0s, 1s and 2s
+void printList(struct Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+void insert(int n1) {
+    int n, value, i;
+    // scanf("%d",&n);
+    n = n1;
+    struct Node *temp;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        if (i == 0) {
+            start = new Node(value);
+            temp = start;
+            continue;
+        } else {
+            temp->next = new Node(value);
+            temp = temp->next;
+            temp->next = NULL;
+        }
+    }
+}
+
+int main() {
+
+    int n;
+
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d", &n);
+
+        insert(n);
+        struct Node *newHead = segregate(start);
+        printList(newHead);
+    }
+
+    return 0;
+}// } Driver Code Ends
+
+
+/*
+  Sort the list of 0's,1's and 2's
+  The input list will have at least one element
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// This function is to segregate the elememtns in the linked list
+// This will do the required arrangement by changing the links
+Node* segregate(Node *head) {
+    
+    // Add code here
+    int c0=0,c1=0,c2=0;
+    Node *ans=head;
+    while(ans!=NULL)
+    {
+        if(ans->data==0)
+            c0++;
+        else if(ans->data==1)
+            c1++;
+        else
+            c2++;
+        ans=ans->next;
+    }
+    ans=head;
+    while(ans!=NULL)
+    {
+        if(c0!=0)
+        {
+            ans->data=0;
+            c0--;
+        }
+        else if(c1!=0)
+        {
+            ans->data=1;
+            c1--;
+        }
+        else if(c2!=0)
+        {
+            ans->data=2;
+            c2--;
+        }
+        ans=ans->next;
+    }
+    return head;
+}
