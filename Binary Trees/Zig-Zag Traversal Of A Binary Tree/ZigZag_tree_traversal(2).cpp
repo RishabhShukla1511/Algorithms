18720e278a32d1d1035b41e833028174ff11ff23
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-03-13 23:25:15 +0530

    Added ZigZag traversal

---
+++ Binary Trees/Zig-Zag Traversal Of A Binary Tree/ZigZag_tree_traversal(2).cpp
@@ -0,0 +1,153 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+vector <int> zigZagTraversal(Node* root);
+
+int main()
+{
+    int t;
+    scanf("%d ",&t);
+  while (t--)
+  {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+        vector <int> res = zigZagTraversal(root);
+        for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
+        cout<<endl;
+  }
+  return 0;
+}
+// Contributed by: Hashit Sidhwa
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+/*Structure of the node of the binary tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// return a vector containing the zig zag level order traversal of the given tree
+vector <int> zigZagTraversal(Node* root)
+{
+	// Code here
+	vector<int>zigzag;
+	if(root)
+	{
+	    queue<Node*>q;
+	    q.push(root);
+	    int level=1;
+	    while(!q.empty())
+	    {
+	        int n=q.size();
+	        vector<int>temp;
+	        for(int i=1;i<=n;i++)
+	        {
+	            root=q.front();
+	            temp.push_back(root->data);
+	            if(root->left)
+	                q.push(root->left);
+	            if(root->right)
+	                q.push(root->right);
+	            q.pop();
+	        }
+	        if(level%2==0)
+	        {
+	            for(int i=temp.size()-1;i>=0;i--)
+	                zigzag.push_back(temp[i]);
+	        }
+	        else
+	        {
+	            for(int i=0;i<temp.size();i++)
+	                zigzag.push_back(temp[i]);
+	        }
+	        level++;
+	    }
+	}
+	return zigzag;
+}
