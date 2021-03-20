38be32acfa6ddfd5613d54af94f5a797e29897c7
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-03-20 23:44:04 +0530

    Added Number of connected components

---
+++ Graph/Number Of Connected Components/Number_of_connected_components(1).cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void make_set(int parent[],int rank[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        parent[i]=i;
+        rank[i]=0;
+    }
+}
+
+int find_set(int parent[],int ver)
+{
+    if(ver==parent[ver])
+        return ver;
+    return parent[ver]=find_set(parent,parent[ver]);
+}
+
+int numConnectedComp(vector<pair<int,int>>v,int n)
+{
+    int rank[n],parent[n];
+    make_set(parent,rank,n);
+    int e=v.size();
+    for(int i=0;i<e;i++)
+    {
+        int s=v[i].first,d=v[i].second,a=find_set(parent,s),b=find_set(parent,d);
+        if(a!=b)
+        {
+            if(rank[a]<rank[b])
+                swap(a,b);
+            parent[b]=a;
+            if(rank[a]==rank[b])
+                rank[a]++;
+        }
+    }
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        if(parent[i]==i)
+            ans++;
+    }
+    return ans;
+}
+
+int main()
+{
+    int V,E;
+    cout << "Enter the number of vertices\n";
+    cin >> V;
+    cout << "Enter the number of edges\n";
+    cin >> E;
+    vector<pair<int,int>>v;
+    cout << "Enter the edges\nVertices are labelled from 0\n";
+    int k=0;
+    for(int i=1;i<=E;i++)
+    {
+        cout << "Enter the start and end vertex\n";
+        int s,d;
+        cin >> s >> d;
+        if(s<0 || s>=V || d<0 || d>=V)
+        {
+            cout << "Invlalid entry\n";
+            k=1;
+            break;
+        }
+        v.push_back({s,d});
+    }
+    if(k==0)
+        cout << "Number of connected components = " << numConnectedComp(v,V);
+}
---
+++ Graph/Number Of Connected Components/Number_of_connected_components(2).cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<int>v[],int ver,int visited[])
+{
+    visited[ver]=1;
+    for(int i=0;i<v[ver].size();i++)
+    {
+        if(visited[v[ver][i]]==0)
+            dfs(v,v[ver][i],visited);
+    }
+}
+
+int NoOfConnectedComponents(vector<int>v[],int i,int noofvertices)
+{
+    int visited[noofvertices+1]={0},noofconnectedcomp=0,level=0;
+    for(int i=1;i<=noofvertices;i++)
+    {
+        if(visited[i]==0)
+        {
+            dfs(v,i,visited);
+            noofconnectedcomp++;
+        }
+    }
+    return noofconnectedcomp;
+}
+
+int main()
+{
+    int noofedges,noofvertices;
+    cout << "Enter the number of vertices in graph\n";
+    cin >> noofvertices;
+    cout << "Enter the number of edges in graph\n";
+    cin >> noofedges;
+    vector<int>v[noofvertices+1];
+    int k=noofedges;
+    for(int i=1;i<=k;i++)
+    {
+        cout << i << " edge\n";
+        int a,b;
+        cout << "Enter the starting and ending vertex\n";
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    int visited[noofvertices+1]={0};
+
+    int cc=NoOfConnectedComponents(v,1,noofvertices);
+    cout << "Number of connected components = " << cc;
+}
