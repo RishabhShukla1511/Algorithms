fe16630904907866bc183f2f67e3d463a4c9a36f
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-03-24 23:26:58 +0530

    Added Brothers from different roots

---
+++ Binary Search Trees/Count Pairs From 2 BST Whose Sum Is Equal To Given Value X/Brothers_from_different_roots(3).cpp
@@ -0,0 +1,101 @@
+using namespace std;
+
+// structure of a node of BST
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+int countPairs(Node* root1, Node* root2, int x);
+
+// Driver code
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node* root1 = NULL;
+        Node* root2 = NULL;
+        int n1, n2, k;
+        cin>>n1;
+        for(int i=0; i<n1; i++)
+        {
+            cin>>k;
+            insert(&root1, k);
+        }
+        cin>>n2;
+        for(int i=0; i<n2; i++)
+        {
+            cin>>k;
+            insert(&root2, k);
+        }
+        int s;
+        cin>>s;
+        cout<<countPairs(root1, root2, s)<<"\n";
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+/*Structure of the Node of the BST is as
+struct Node {
+	int data;
+	Node* left, *right;
+};*/
+
+void inOrder(Node *root,map<int,int>&m)
+{
+    if(root)
+    {
+        inOrder(root->left,m);
+        m[root->data]++;
+        inOrder(root->right,m);
+    }
+}
+
+// You are required to complete this function
+int countPairs(Node* root1, Node* root2, int x)
+{
+    // Code here
+    map<int,int>m1,m2;
+    inOrder(root1,m1);
+    inOrder(root2,m2);
+    int cnt=0;
+    for(auto it:m1)
+        cnt+=it.second*m2[x-it.first];
+    return cnt;
+}
