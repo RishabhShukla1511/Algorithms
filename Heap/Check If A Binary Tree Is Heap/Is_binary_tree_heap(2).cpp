02d47e3d11db985dbe70516949ef7f78677b179d
RishabhShukla1511 <42274135+RishabhShukla1511@users.noreply.github.com>
2021-04-17 23:30:04 +0530

    Added Check if binary tree is heap

---
+++ Heap/Check If A Binary Tree Is Heap/Is_binary_tree_heap(2).cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+bool isHeap(struct Node * tree);
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+
+     cout << isHeap(root)<< endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+ A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+/*You are required to complete this method */
+bool isHeap(Node * tree)
+{
+ // Your code here
+    if(tree->left!=NULL)
+    {
+        if(tree->data>tree->left->data)
+            return isHeap(tree->left);
+        else
+            return false;
+    }
+    if(tree->right!=NULL)
+    {
+        if(tree->data>tree->right->data)
+            return isHeap(tree->right);
+        else
+            return false;
+    }
+    return true;
+}
